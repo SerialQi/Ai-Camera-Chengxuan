f33261e1af31c5df715d336c0ebaf4fd91508b46
Chengxuan Mei <133566643+SerialQi@users.noreply.github.com>
2024-06-05 17:32:23 +1200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let sourceImg=null;
+let maskImg=null;
+let renderCounter=0;
+
+// change these three lines as appropiate
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
+
+function preload() {
+  sourceImg = loadImage(sourceFile);
+  maskImg = loadImage(maskFile);
+}
+
+function setup () {
+  let main_canvas = createCanvas(1920, 1080);
+  main_canvas.parent('canvasContainer');
+
+  imageMode(CENTER);
+  noStroke();
+  background(255, 0, 0);
+  sourceImg.loadPixels();
+  maskImg.loadPixels();
+}
+
+function draw () {
+  for(let i=0;i<4000;i++) {
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    fill(pix);
+    if(mask[0] > 128) {
+      let pointSize = 10;
+      ellipse(x, y, pointSize, pointSize);
+    }
+    else {
+      let pointSize = 20;
+      rect(x, y, pointSize, pointSize);    
+    }
+  }
+  renderCounter = renderCounter + 1;
+  if(renderCounter > 10) {
+    console.log("Done!")
+    noLoop();
+    // uncomment this to save the result
+    // saveArtworkImage(outputFile);
+  }
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+}
